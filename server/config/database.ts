200ba993bcb204191a56295c0ba35bf785e93c20
JONATHAN DIAS <jonathan-dias@outlook.fr>
2025-07-28 16:05:06 +0200

    fix: add port for railway

--- server/config/database.ts
+++ server/config/database.ts
@@ -7,6 +7,7 @@ const dbName = process.env.DB_NAME || process.env.MYSQLDATABASE;
 const dbUser = process.env.DB_USER || process.env.MYSQLUSER;
 const dbPassword = process.env.DB_PASSWORD || process.env.MYSQLPASSWORD;
 const dbHost = process.env.DB_HOST || process.env.MYSQLHOST;
+const dbPort = Number(process.env.DB_PORT || process.env.MYSQLPORT || 3306);
 
 const sequelize = new Sequelize(
   dbName as string,
@@ -14,6 +15,7 @@ const sequelize = new Sequelize(
   dbPassword as string,
   {
     host: dbHost,
+    port: dbPort,
     dialect: "mysql",
     logging: false,
     define: {
