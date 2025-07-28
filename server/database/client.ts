e4ff29d00330716143984c1cb10cdf0f8bb72014
JONATHAN DIAS <jonathan-dias@outlook.fr>
2025-07-28 15:44:23 +0200

    feat: add vercel and railways config to deploy front and back

--- server/database/client.ts
+++ server/database/client.ts
@@ -1,15 +1,26 @@
 // Get variables from .env file for database connection
-const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+const {
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  MYSQLHOST,
+  MYSQLPORT,
+  MYSQLUSER,
+  MYSQLPASSWORD,
+  MYSQLDATABASE,
+} = process.env;
 
 // Create a connection pool to the database
 import mysql from "mysql2/promise";
 
 const client = mysql.createPool({
-  host: DB_HOST,
-  port: Number.parseInt(DB_PORT as string),
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME,
+  host: DB_HOST || MYSQLHOST,
+  port: Number.parseInt((DB_PORT || MYSQLPORT) as string),
+  user: DB_USER || MYSQLUSER,
+  password: DB_PASSWORD || MYSQLPASSWORD,
+  database: DB_NAME || MYSQLDATABASE,
 });
 
 // Ready to export
